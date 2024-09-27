87071086f4617b491583f443b0cf066d348bad50
sidhardha58 <sidhu.valavala@gmail.com>
2024-09-27 10:34:06 +0530

    Data Types

---
+++ dataTypes.js
@@ -0,0 +1,46 @@
+// INT
+var a = 58;
+console.log(a); // 58
+
+// Sting
+let b = "JavaScript";
+console.log(b); // "JavaScript"
+
+// Bool
+const isvalid = false;
+console.log(isvalid); // false
+
+// typeof function
+const num = 14;
+console.log(typeof num); // "number"
+
+const user = "sidhu";
+console.log(typeof user); // "string"
+
+const valid = true;
+console.log(typeof valid); // "boolean"
+
+//Object
+my_object = {
+  user: "JohnDoe",
+  id: 100,
+  password: "john123",
+};
+console.log(typeof my_object); // "object"
+
+//Array -> Special-Object
+const my_array = ["Spider-Man", "Hulk", "Black Panther"];
+console.log(typeof my_array); // "object"
+
+// Over-writing
+let first_val = 42;
+console.log(first_val); // 42
+first_val = 88;
+console.log(first_val); // 88
+
+// Constant
+const constant = 99;
+console.log(constant); // 99
+
+// Reassigning value to const would throw an error:
+// constant = 101; -> ERROR: Assignment to constant variable
