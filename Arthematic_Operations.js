d5d4168a3f4d59235dd75df46883f252ecd3eed5
sidhardha58 <sidhu.valavala@gmail.com>
2024-09-28 21:20:07 +0530

    Arthematic Operations

---
+++ Arthematic_Operations.js
@@ -0,0 +1,17 @@
+let a = 75;
+let b = 25;
+
+const sum = a + b; //Addition
+console.log("The sum is: ", sum); //100
+
+const diff = a - b; //Substraction
+console.log("The difference is: ", diff); //50
+
+const mul = a * b; //Multiplication
+console.log("The product is: ", mul); //1875
+
+const div = a / b; //Division
+console.log("The division is: ", div); //3
+
+const rem = a % b; //Modulos
+console.log("The remainder is: ", rem); //0
