1c698eb8485e6253ed99fdcad8630ccc42ec64e2
sidhardha58 <sidhu.valavala@gmail.com>
2024-09-29 18:59:44 +0530

    Increment and Decrement Variables

---
+++ Increment_Decrement.js
@@ -0,0 +1,15 @@
+//INCREMENT
+
+let initial = 200; // Initialize the variable `initial` with a value of 200
+console.log("Initial value: ", initial); // Output : 200
+
+initial += 50; // Increment `initial` by 50 and store the new value in the same variable
+console.log("Value after Incrementation: ", initial); // Output : 250
+
+//DECREMENT
+
+initial = 200; // Reassign `initial` back to 200
+console.log("Initial value: ", initial); // Output : 200
+
+initial -= 50; // Decrement `initial` by 50 and store the new value in the same variable
+console.log("Value after Decrementation: ", initial); // Output : 150
