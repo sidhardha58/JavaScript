07f6b75264f54a99471cc47a4b4bd8a8fdd60320
Sidhardha V <sidhu.valavala@gmail.com>
2024-10-07 22:41:52 +0530

    Leapyear check

---
+++ LeapYear.js
@@ -0,0 +1,7 @@
+let year = 2024;
+
+if (year % 4 == 0 && year % 100 != 0) console.log("LeapYear");
+else if (year % 400 == 0) console.log("LeapYear");
+else console.log("Not LeapYear");
+
+// Output : Leapyear
