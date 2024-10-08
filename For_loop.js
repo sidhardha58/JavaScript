66aac8104dc7447e4e76782b107de189f29bb7bf
Sidhardha V <sidhu.valavala@gmail.com>
2024-10-08 22:29:38 +0530

    For Loop

---
+++ For_loop.js
@@ -0,0 +1,9 @@
+for (let index = 1; index <= 10; index++) {
+  console.log(index);
+}
+//Output : 1 2 3 4 5 6 7 8 9 10
+
+for (let index = 10; index > 0; index--) {
+  console.log(index);
+}
+//Output : 10 9 8 7 6 5 4 3 2 1
