7a6a15a7f3ed359dd12748e57c3678a0ce14f08f
Sidhardha V <sidhu.valavala@gmail.com>
2024-10-10 22:55:22 +0530

    While loop

---
+++ while_loop.js
@@ -0,0 +1,13 @@
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+//Output : 1 2 3 4 5 6 7 8 9 10
+
+i = 10;
+while (i >= 1) {
+  console.log(i);
+  i--;
+}
+//Output : 10 9 8 7 6 5 4 3 2 1
