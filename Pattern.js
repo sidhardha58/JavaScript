12790fbbcaa7ddd721f88ebe01b185398ad896c1
Sidhardha V <sidhu.valavala@gmail.com>
2024-10-11 22:48:50 +0530

    Star pattern

---
+++ Pattern.js
@@ -0,0 +1,17 @@
+for (let i = 1; i <= 5; i++) {
+  let k = "";
+  for (j = 0; j < i; j++) {
+    k += "*\t";
+  }
+  console.log(k);
+}
+
+/* Output :
+
+ *
+ *  *
+ *  *  *
+ *  *  *  *
+ *  *  *  *  *
+ 
+*/
