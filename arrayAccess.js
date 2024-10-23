d1e2f65eb72155cdd1888793de71a45c0fedf26c
Sidhardha V <sidhu.valavala@gmail.com>
2024-10-23 22:30:24 +0530

    Access elements in array

---
+++ arrayAccess.js
@@ -0,0 +1,4 @@
+my_arr = [1, 2, 3, 4, 5];
+
+console.log(`The first element of the array is ${my_arr[0]}`); //Output : 1
+console.log(`The last element of the array is ${my_arr[my_arr.length - 1]}`); //Output : 5
