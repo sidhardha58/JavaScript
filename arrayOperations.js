94a683036f61bb9fe7b59bfb1367d7c03239141e
Sidhardha V <sidhu.valavala@gmail.com>
2024-10-24 23:21:37 +0530

    Array operations

---
+++ arrayOperations.js
@@ -0,0 +1,18 @@
+my_arr = [1, 2, 3, 4, 5, 6];
+console.log(my_arr);
+
+//Push operation
+my_arr.push(7);
+console.log(my_arr);
+
+// Pop operation
+my_arr.pop();
+console.log(my_arr);
+
+//Shift operation
+my_arr.shift();
+console.log(my_arr);
+
+//Unshift operation
+my_arr.unshift(1);
+console.log(my_arr);
