a38889f468b8b458482a880cf32f873eaab97528
Sidhardha V <sidhu.valavala@gmail.com>
2024-10-29 22:27:21 +0530

    Printing array elements

---
+++ arrayPrint.js
@@ -0,0 +1,11 @@
+my_arr = [1, 2, 3, 4, 5, 6, 7];
+
+//For loop
+for (let i = 0; i < my_arr.length; i++) console.log(my_arr[i]);
+
+//For-Each loop
+my_arr.forEach((element) => {
+  console.log(element);
+});
+
+//Output : 1 2 3 4 5 6 7
