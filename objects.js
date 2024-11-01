61894e809c279e28db57067618e4fd7ad3ef4042
Sidhardha V <sidhu.valavala@gmail.com>
2024-11-01 22:07:49 +0530

    Objects in JavaScript

---
+++ objects.js
@@ -0,0 +1,14 @@
+my_book = {
+  title: "Rich Dad Poor Dad",
+  author: "Robert",
+  year: 1997,
+};
+
+console.log(my_book);
+// Output : { title: 'Rich Dad Poor Dad', author: 'Robert', year: 1997 }
+
+console.log(`The title of the book is ${my_book.title}`);
+console.log(`The author of the book is ${my_book.author}`);
+
+//Output : The title of the book is Rich Dad Poor Dad
+//Output : The author of the book is Robert
