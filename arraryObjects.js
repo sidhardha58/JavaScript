d340b83e0d411d6983a722db4864f4d9f50eda32
Sidhardha V <sidhu.valavala@gmail.com>
2024-11-04 22:51:32 +0530

    Library Objects

---
+++ arraryObjects.js
@@ -0,0 +1,22 @@
+let library = {
+  name: "Ramanujan Library",
+  books: [
+    {
+      title: "Rich Dad Poor Dad",
+      author: "Robert",
+      year: 1997,
+    },
+    {
+      title: "To kill a MockingBird",
+      author: "Harper lee",
+      year: 1960,
+    },
+    {
+      title: "The Lord of Rings",
+      author: "John Ronald",
+      year: 1937,
+    },
+  ],
+};
+
+console.log(library);
