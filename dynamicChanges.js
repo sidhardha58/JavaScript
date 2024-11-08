080047736047d8a4375766f074c5731e7a96174c
Sidhardha V <sidhu.valavala@gmail.com>
2024-11-08 20:28:27 +0530

    Dynamic representation using backticks'

---
+++ dynamicChanges.js
@@ -0,0 +1,7 @@
+let name = "AB de Villiers";
+let age = 39;
+let profession = "Cricket";
+
+const literal = `${name} is one of the greatest in the profession of ${profession}. He is known for his exceptional skills and innovative shots, remaining a legendary finisher even at the age of ${age}.`;
+
+console.log(literal);
