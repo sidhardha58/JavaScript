950fabfbd086d6440ddcf5bd1466b8a6f3fa6efb
Sidhardha V <sidhu.valavala@gmail.com>
2024-11-13 22:36:40 +0530

    Object destructuring

---
+++ objectDestructuring.js
@@ -0,0 +1,8 @@
+let book = {
+  title: "Rich Dad Poor Dad",
+  author: "Robert",
+  year: 1997,
+};
+
+const { title, author, year } = book;
+console.log(`The title of the book is ${title} and the author is ${author}`);
