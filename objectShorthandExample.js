efb826342e35e88fefd384f2a6e013b0c127a8de
Sidhardha V <sidhu.valavala@gmail.com>
2024-12-02 23:56:27 +0530

    Example of object shorthand properties and methods

---
+++ objectShorthandExample.js
@@ -0,0 +1,14 @@
+const name = "Sidhu";
+const age = 19;
+const email = "sidhu.valavala@gmail.com";
+
+const user = {
+  name,
+  age,
+  email,
+  islogged() {
+    console.log("The user is logged in");
+  },
+};
+
+console.log(user);
